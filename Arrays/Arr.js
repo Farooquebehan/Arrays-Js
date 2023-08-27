6ffd5c8e6d09dac22f840a9d933d9facd7b44973
Farooq Behan <56262805+Farooquebehan@users.noreply.github.com>
2023-08-27 08:07:59 +0000

    container config done

---
+++ Arrays/Arr.js
@@ -0,0 +1,18 @@
+const myArr = [1,2,4,5]
+const myHeros = ["superman" , "spiderman"]
+// console.log(myArr[1]);
+// console.log(myHeros[1]);
+
+// const myArr2 = new Ary(1,2,3,4)
+console.log(myArr[1]);
+
+//Array Methods 
+// myArr.push(6)
+// myArr.push(8)                       
+// myArr.pop();
+// myArr.unshift(9);
+// myArr.shift();
+// console.log(myArr);
+const newArr = myArr.join()
+console.log(myArr);
+console.log(typeof myArr); 
---
+++ Arrays/Arr1.js
@@ -0,0 +1,24 @@
+const marvel_Heros = ["spiderman", "ironman","thor"]
+const dc_heros = ["superman","flash","batman"]
+
+// marvel_Heros.push(dc_heros)
+
+// console.log(marvel_Heros[3][1]);
+// const allheros =  marvel_Heros.concat(dc_heros)
+// console.log(allheros);
+// const all_new_heros = [...marvel_Heros,...dc_heros]
+// console.log(all_new_heros);
+
+// const another_arry = [1,2,3,[7,8,9],8,[5,6,7],2,[12,13,]]
+// const  real_another_arr =  another_arry.flat(Infinity)
+
+// console.log(real_another_arr);
+// console.log(Array.isArray("farook"));
+// console.log(Array.from("farook"));
+// console.log(Array.from({name:"Farook"})) // interesting interview point of view
+ let score1 = 100;
+ let score2 = 200;
+ let score3 = 300;
+ console.log(Array.of(score1, score2,score3));
+
+
